d026f716cb7fe992c272f5e16ca92b6ac47c9c08
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-10 01:54:09 +0200

    feat: add createAnime controller with automatic user binding

--- src/controllers/anime.controller.ts
+++ src/controllers/anime.controller.ts
@@ -1,6 +1,28 @@
 import type { Request, Response } from "express";
+import BadRequest from "../errors/badRequest.js";
+import Anime from "../models/Anime.js";
+import { StatusCodes } from "http-status-codes";
 
-export const createAnime = async (req: Request, res: Response) => {};
+export const createAnime = async (req: Request, res: Response) => {
+  const { userId } = req.userData!;
+
+  const { title, image, animeId, createdBy } = req.body;
+
+  if (!title || !image || !animeId || !createdBy) {
+    throw new BadRequest("Please provide all values");
+  }
+
+  const anime = await Anime.create({
+    title,
+    image,
+    animeId,
+    createdBy: userId,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ data: anime, message: "Anime created successfully" });
+};
 
 export const getAnime = async (req: Request, res: Response) => {};
 
