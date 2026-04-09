a2bc6d2b8f5a721ce7833666e117d5d0b0954277
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-09 22:52:15 +0200

    feat: implement errorHandler and notFound middlewares with custom TypeScript interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,32 @@
+import "dotenv/config";
+import "express-async-errors";
+
 import express from "express";
+import cors from "cors";
+import notFound from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
---
+++ src/erros/index.ts
@@ -0,0 +1,13 @@
+import CustomAPIError from "./customError.js";
+import BadRequest from "./badRequest.js";
+import UnauthenticatedError from "./unauthenticated.js";
+import UnAuthorizedError from "./unauthorized.js";
+import NotFoundError from "./notFound.js";
+
+export {
+  CustomAPIError,
+  BadRequest,
+  UnauthenticatedError,
+  UnAuthorizedError,
+  NotFoundError,
+};
