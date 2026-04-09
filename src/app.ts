aa1ad84426ac3f7e5de8ee49c44c82c67fb4d4b5
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-09 23:00:30 +0200

    feat: establish MongoDB connection using Mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import "express-async-errors";
 
-import express from "express";
 import cors from "cors";
-import notFound from "./middleware/not-found.js";
+import express from "express";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFound from "./middleware/not-found.js";
+import connectDB from "./utils/connectDB.js";
 
 const app = express();
 
@@ -30,6 +31,11 @@ app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+    await connectDB(process.env.MONGO_URI);
+    console.log("Connected DB!");
     app.listen(port, () => console.log("Server is listening on port: " + port));
   } catch (error) {
     console.log(error);
