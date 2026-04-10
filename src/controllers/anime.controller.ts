6802fd3936f833b9d9924131e1d1c86452e96947
Tanju Özer <tanju.ozer64@gmail.com>
2026-04-10 02:03:38 +0200

    feat: implement paginated getAnime controller

--- src/controllers/anime.controller.ts
+++ src/controllers/anime.controller.ts
@@ -24,6 +24,31 @@ export const createAnime = async (req: Request, res: Response) => {
     .json({ data: anime, message: "Anime created successfully" });
 };
 
-export const getAnime = async (req: Request, res: Response) => {};
+export const getAnime = async (req: Request, res: Response) => {
+  const { userId } = req.userData!;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 12;
+  const skip = (page - 1) * limit;
+
+  const userAnime = await Anime.find({ createdBy: userId })
+    .sort("-createdAt")
+    .skip(skip)
+    .limit(limit);
+
+  const totalAnimes = await Anime.countDocuments({ createdBy: userId });
+
+  const numOfPages = Math.ceil(totalAnimes / limit);
+
+  res.status(StatusCodes.OK).json({
+    data: userAnime,
+    pagination: {
+      totalAnimes,
+      numOfPages,
+      currentPage: page,
+      has_next_page: page < numOfPages,
+    },
+  });
+};
 
 export const deleteAnime = async (req: Request, res: Response) => {};
